64cf278444806ed15bcde52c7f359b05ccbe5a25
Birat shrestha <110214490+birat2400@users.noreply.github.com>
2023-08-02 17:57:05 +0545

    Add files via upload

---
+++ MERN/index.js
@@ -0,0 +1,52 @@
+// alert("Hellow world");
+// const name = prompt("ENter your useername");
+// const pw = prompt("enter your password");
+
+// if (pw === "birathoo"){
+//     alert("Welcome")
+// }
+// console.log(a);
+
+
+// const hour = prompt("Enter the time");
+// if(hour > 14 && hour < 18)
+// {
+//     alert("js class is running");
+// }
+
+// const isvalid = false;
+// if(!isvalid)
+// {
+//     alert("it is not valid");
+// }
+
+// ternary operator
+// let a=2;
+
+// if (a>0) {
+//     alert("a is greater then 0");
+// }
+// else {
+//     alert("a is less then 0");
+// }
+// a>5 ? alert("a is greater then 0") : alert("a is less then 0");
+
+// statement(for loop)
+
+// for(let a = 0; a < 10; a++)
+// {
+//     console.log(a);
+// }
+// let a=0;
+// while(a<10) {
+
+//     console.log(a);
+// a++;
+// }
+let a=0;
+do{
+
+    console.log(a);
+a++;
+}
+while(a<=10);
